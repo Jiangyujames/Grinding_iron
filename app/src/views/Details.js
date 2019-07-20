3db105856bb3d9f8f1956773c4b5ff6ac370d3f4
una <13661193349@163.com>
2019-07-20 20:42:42 +0800

    详情页面样式添加

--- app/src/views/Details.js
+++ app/src/views/Details.js
@@ -31,36 +31,56 @@ class Detail extends React.Component{
 			 console.log(this.state.bookInfo['tuijianBookList'],44444444444444)
 		    return (
 			<div className={"detals"}>
-					   <div className={"deta"}>
-					   <span className={"back"} onClick={()=>this.props.history.go(-1)}>&lt;</span><span className={"tite"}>作品详情</span></div>
-							<img  className={"book"}src={this.state.bookInfo.icon} />
-							<div>
-				 			{this.state.bookInfo.name}
-				 			<p>{this.state.bookInfo.authorName}|{this.state.bookInfo.sortName}|{this.state.bookInfo.status}</p>
-							<p>{this.state.bookInfo.words}{this.state.bookInfo.discountPrice}</p>
+					<div className={"deta"}>
+					 <span className={"back"} onClick={()=>this.props.history.go(-1)}>&lt;</span><span className={"tite"}>作品详情</span></div>
+					 
+					 
+					<div className={"bg"} style={{background:this.state.bookInfo.icon}}>		
+					   <div className="infot">
+						<img  className={"book"} src={this.state.bookInfo.icon} />
+					    <div className={"inforight"}>
+							<h3 className={"bname"}>{this.state.bookInfo.name}</h3>
+							<p>{this.state.bookInfo.authorName} | {this.state.bookInfo.sortName} | {this.state.bookInfo.status}</p>
+							<p>{this.state.bookInfo.words}字 | {this.state.bookInfo.discountPrice}</p>
 							<p>粉丝数:{this.state.bookInfo.followCount}</p>
 							<p>点击数:{this.state.bookInfo.visitCount}</p>
-				 			</div>
-							<hr/>
-							<input type="button"  value={"加入书架"} onClick={()=>{   
+					      </div>
+					</div>
+					
+					</div>
+					   
+
+							<div className={"btns"}>
+							<input type="button"  className={"add"} value={"加入书架"} onClick={()=>{   
 														   console.log("加入书架");	   
 										                }}/>
-			                <input type="button"  value={"立即阅读"} onClick={()=>{   
+			                <input type="button"  className={"read"} value={"立即阅读"} onClick={()=>{   
 			                	// this.props.history.push("/read");
 			                   console.log("立即阅读");	   
 			                }}/>
+							</div>
+							
+							<div className={"dashang"}>
+								<p><i></i><p>打赏</p></p>
+								<p><i></i><p>推荐</p></p>
+								</div>
 							<hr/>
-							<div><i>打赏</i><i>推荐</i></div>
-							<div>{this.state.bookInfo.introduce}</div>
-							<div><i>目录:</i>{console.log(this.state.bookInfo['lastChapterList'])}<i>连载至</i></div>
-							<p>看过这本书的人还看过</p>
-						{
+							<div className={"introduce"}>{this.state.bookInfo.introduce}</div>
+							
+							<div className={"mulu"}><em></em><p>目录:</p>{console.log(this.state.bookInfo['lastChapterList'])}<p>连载至</p></div>
+							<p><em></em>看过这本书的人有</p>
+						   
+						   <div className={"more"}> {
 							!this.state.bookInfo['reviewList']?null:this.state.bookInfo['reviewList'].map((v,i)=>{
 								return(
-								<div key={i}><img src={v.userIcon}/></div>
+								<div className={"people"} key={i}>
+								<img src={v.userIcon}/>
+								<p>{v.userName}</p>									
+								</div>
 								)
 							})
-						}						
+							  }	
+							</div>
 			</div>
 
 		    )
